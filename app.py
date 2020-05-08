51316460f87963e38d09621203101efdbb221ad9
piotrmstaniszewski <piotr.mstaniszewski@gmail.com>
2020-05-08 17:41:48 +0200

    trying out converters

--- app.py
+++ app.py
@@ -21,6 +21,19 @@ def hello_world():
 def search(search_query):
     return search_query
 
+# now try out converters
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print(value + 1)
+    return "correct"
+@app.route("/float/<float:value>"):
+def float_type(value):
+    print(value + 1)
+    return "correct"
+@app.route("/path/<path:value>"):
+    print(value)
+    return "correct"
+
 # start the development server using the run() method
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
