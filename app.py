eb2b3e13a02b7cf07c8c88a744233680a55eaae4
piotrmstaniszewski <piotr.mstaniszewski@gmail.com>
2020-05-08 15:39:55 +0200

    if function corrected

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ def hello_world():
     return "Hello world!"
 
 # start the development server using the run() method
-if __name__ = "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
